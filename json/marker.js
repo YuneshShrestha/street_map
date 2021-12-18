f5e71a424ca7b0eb56a124185ac37508cd0f07a0
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-18 10:56:46 +0545

    Geo Json and markers

---
+++ json/marker.js
@@ -0,0 +1,85 @@
+let markerJson={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "color": "#7e7e7e",
+          "size": "medium",
+          "symbol": "",
+          "location": "katari"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            86.23168945312499,
+            27.010196431931526
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "color": "#7e7e7e",
+          "size": "medium",
+          "symbol": "",
+          "location": "kavrae"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            85.6109619140625,
+            27.522886832325796
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "color": "#7e7e7e",
+          "size": "medium",
+          "symbol": "",
+          "location": "Hetauda Street"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            85.02319335937499,
+            27.364449817684083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "color": "#7e7e7e",
+          "size": "medium",
+          "symbol": "",
+          "location": "Damauli Street Nepal"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            84.39697265625,
+            28.13981591275445
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "color": "#7e7e7e",
+          "size": "medium",
+          "symbol": "",
+          "location": "Darbang Street Nepal"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            83.3477783203125,
+            28.442128910370474
+          ]
+        }
+      }
+    ]
+  }
